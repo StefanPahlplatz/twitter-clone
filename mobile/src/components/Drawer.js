a95ad0d245c55adb8fcd3628f2316d642e7292ac
Stefan Pahlplatz <stefan314@live.nl>
2017-08-16 22:46:09 +0200

    Custom drawer.

---
+++ mobile/src/components/Drawer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { DrawerItems } from 'react-navigation';
+
+const Root = styled.View`flex: 1;`;
+
+const Drawer = props => <Root />;
+
+export default Drawer;
--- mobile/src/navigation/MainDrawerNavigator.js
+++ mobile/src/navigation/MainDrawerNavigator.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MainStackNavigator from './MainStackNavigator';
+import Drawer from '../components/Drawer';
 
-const MainDrawerNavigator = DrawerNavigator({
-  DrawerItem1: {
-    screen: MainStackNavigator,
-    navigationOptions: {
-      drawer: {
-        label: 'Drawer 1',
-        icon: ({ tintColor }) => <Icon name="rocket" size={24} />,
+const MainDrawerNavigator = DrawerNavigator(
+  {
+    DrawerItem1: {
+      screen: MainStackNavigator,
+      navigationOptions: {
+        drawerLabel: 'topkek',
       },
     },
   },
-});
+  {
+    contentComponent: props => <Drawer {...props} />,
+  }
+);
 
 export default MainDrawerNavigator;
